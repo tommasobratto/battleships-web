2cd917aaffb5cc542a5c6fdf67715720f08cbf4f
Kate Beavis <katebeavis84@gmail.com>
2015-02-17 19:46:47 +0000

    Got submit button working

--- features/starting_a_game.feature
+++ features/starting_a_game.feature
@@ -3,7 +3,14 @@ Feature: Starting the game
   As a nostalgic player
   I want to start a new game
 
-  Scenario: Registering 
+ Scenario: Registering 
     Given I am on the homepage
     When I follow "New Game"
     Then I should see "What's your name?"
+
+ Scenario: No name given
+ 	Given I am on the player page
+ 	And I don't type a "name"
+ 	When I click on "submit"
+ 	Then I should see "Please enter a name"
+
--- features/step_definitions/starting_a_game_steps.rb
+++ features/step_definitions/starting_a_game_steps.rb
@@ -9,3 +9,20 @@ end
 Then(/^I should see "(.*?)"$/) do |arg1|
   page.should have_content("What's your name?")
 end
+
+Given(/^I am on the player page$/) do
+  visit('/player')
+end
+
+And(/^I don't type a "(.*?)"$/) do |name|
+  fill_in 'name', :with => ''
+end
+
+When(/^I click on "(.*?)"$/) do |link|
+  click_on(link)
+end
+
+Then(/^I should be "(.*?)"$/) do |arg1|
+  page.should have_content("Please enter a name")
+end
+
